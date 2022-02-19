321490713d64bc38d9f6affd3b97c3a490f5c7f4
kaylanl <kubraayla.nl@gmail.com>
2022-02-19 18:38:38 +0100

    before Jenkins

---
+++ src/test/java/com/vytrack/runners/CukesRunner.java
@@ -0,0 +1,18 @@
+package com.vytrack.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)        //tell runner where .features file is located
+@CucumberOptions(               //to adjust my executions settings
+        plugin = {"json:target/cucumber.json",
+                "html:target/default-html-reports",
+                "rerun:target/rerun.txt"},
+        features = "src/test/resources/features",
+        glue = "com/vytrack/step_definitions",
+        dryRun = false,
+        tags = "@wip"
+)
+public class CukesRunner {
+}
